b72d7baa2107c5ada8716bab38c03018180031fd
IoTicos <ioticos.org@gmail.com>
2019-11-07 16:44:37 +0000

    capitulo rutas #10

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -9,6 +9,22 @@ class Home extends BaseController
 		echo view('content_home');
 		echo view('footer');
 	}
-	//--------------------------------------------------------------------
+
+	public function history()
+	{
+		echo view('head');
+		echo view('header');
+		echo view('content_history');
+		echo view('footer');
+	}
+
+	public function realtime()
+	{
+		echo view('head');
+		echo view('header');
+		echo view('content_realtime');
+		echo view('footer');
+	}
+
 
 }
